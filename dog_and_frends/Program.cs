d54a83dad153765639871d1b155c900f9356697a
Ilya Korshunov <korshunov.work@gmail.com>
2023-10-11 02:54:54 +0300

    Added graphic

--- dog_and_frends/Program.cs
+++ dog_and_frends/Program.cs
@@ -1,18 +1,36 @@
-﻿Console.Write("Input distance(meters): ");
-int distance = Convert.ToInt32(Console.ReadLine());
+﻿Console.Clear();
+int distance = 10001;
+int distance2 = distance;
 
-Console.Write("Input speed of first friend(m/sec): ");
+while (distance > 10000 || distance < 0)
+{
+    Console.Clear();
+    Console.Write("Input distance (meters. <= 10000): ");
+    distance = Convert.ToInt32(Console.ReadLine());
+    distance2 = distance;
+}
+ 
+Console.Write("Input speed of first friend (m/sec): ");
 int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input speed of second friend(m/sec): ");
+Console.Write("Input speed of second friend (m/sec): ");
 int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input speed of a dog(m/sec): ");
+Console.Write("Input speed of a dog (m/sec): ");
 int dogSpeed = Convert.ToInt32(Console.ReadLine());
 
-int count = 0, friend = 2, time = 0;
+int count = 0, friend = 2, time = 0, time2 = 0, x = 40;
 
-while (distance > 10)
+Console.Clear();
+Console.SetCursorPosition(0, 1);
+Console.WriteLine("8");
+Console.SetCursorPosition((distance / x), 1);
+Console.WriteLine("8");
+Console.SetCursorPosition(1, 1);
+Console.WriteLine("@");
+await Task.Delay(1000);
+
+while (distance > 100)
 {
     if (friend == 1)
     {
@@ -24,8 +42,30 @@ while (distance > 10)
         time = distance / (secondFriendSpeed + dogSpeed);
         friend = 1;
     }
+
     distance -= (firstFriendSpeed + secondFriendSpeed) * time;
     count++;
+    time2 = time2 + time;
+
+    Console.Clear();
+    Console.SetCursorPosition(1, 1);
+    if (friend == 2)
+    {
+        Console.SetCursorPosition(((0 + time2 * firstFriendSpeed) / x) + 1, 1);
+        Console.WriteLine("@");      
+    }
+    else
+    {
+        Console.SetCursorPosition(((distance2 - time2 * secondFriendSpeed) / x) - 1, 1);
+        Console.WriteLine("@");
+    }
+    
+    Console.SetCursorPosition((0 + time2 * firstFriendSpeed) / x, 1);
+    Console.WriteLine("8");
+    Console.SetCursorPosition((distance2 - time2 * secondFriendSpeed) / x, 1);
+    Console.WriteLine("8");
+
+    await Task.Delay(1000);
 }
 
-Console.WriteLine("THe dog ran " + count + " times.");
+Console.WriteLine("The dog ran " + count + " times.");
