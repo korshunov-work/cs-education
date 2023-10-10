ef916db027a2ad16ab3dd5a8346d8d70f3e74c23
Ilya Korshunov <korshunov.work@gmail.com>
2023-10-11 00:31:21 +0300

    Initial commit

---
+++ bubble_sort/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array = {6,7,9,1,2,5,3};
+int transfer;
+bool check = true;
+
+while (check == true)
+{
+    check = false;
+    for (int i = 0; i < (array.Length - 1); i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            transfer = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = transfer;
+            check = true;
+        }
+    }
+}
+
+foreach (int i in array)
+{
+    Console.Write(i + " ");
+}
