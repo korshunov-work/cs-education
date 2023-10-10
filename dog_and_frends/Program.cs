dafcc4f86b5bb192f1f4ebdaa1b3c2829550e52d
Ilya Korshunov <korshunov.work@gmail.com>
2023-10-11 01:39:47 +0300

    Added DogAndFriends project

---
+++ dog_and_frends/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Input distance(meters): ");
+int distance = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input speed of first friend(m/sec): ");
+int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input speed of second friend(m/sec): ");
+int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input speed of a dog(m/sec): ");
+int dogSpeed = Convert.ToInt32(Console.ReadLine());
+
+int count = 0, friend = 2, time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance -= (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine("THe dog ran " + count + " times.");
