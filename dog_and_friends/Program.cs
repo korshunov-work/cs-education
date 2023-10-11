4d39e48a3479ac446c174bccee826416e0720ac0
IlyaKorshunov <korshunov.work@gmail.com>
2023-10-11 18:12:15 +0300

    Upgrade graphic part

--- dog_and_friends/Program.cs
+++ dog_and_friends/Program.cs
@@ -1,15 +1,34 @@
 ﻿Console.Clear();
+
 int distance = 10001;
-int distance2 = distance;
+int distanceBetween = distance;
+int scale = 200;
 
 while (distance > 10000 || distance < 0)
 {
     Console.Clear();
     Console.Write("Input distance (meters. <= 10000): ");
     distance = Convert.ToInt32(Console.ReadLine());
-    distance2 = distance;
+    distanceBetween = distance;
 }
- 
+
+if (distance <= 1000)
+{
+    scale = 10;
+}
+if (distance > 1000 && distance <= 5000)
+{
+    scale = 25;
+}
+if (distance > 5000 && distance <= 10000)
+{
+    scale = 50;
+}
+if (distance > 10000)
+{
+    scale = 100;
+}
+
 Console.Write("Input speed of first friend (m/sec): ");
 int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
 
@@ -19,15 +38,25 @@ int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input speed of a dog (m/sec): ");
 int dogSpeed = Convert.ToInt32(Console.ReadLine());
 
-int count = 0, friend = 2, time = 0, time2 = 0, x = 40;
+int count = 0, friend = 2, time = 0, time2 = 0;
 
 Console.Clear();
 Console.SetCursorPosition(0, 1);
-Console.WriteLine("8");
-Console.SetCursorPosition((distance / x), 1);
-Console.WriteLine("8");
+Console.WriteLine("▓");
+Console.SetCursorPosition(distance / scale, 1);
+Console.WriteLine("▓");
 Console.SetCursorPosition(1, 1);
 Console.WriteLine("@");
+
+for (int i = 0; i < distanceBetween / scale; i++)
+{
+    Console.SetCursorPosition(i, 2);
+    Console.WriteLine("-");
+}
+
+Console.SetCursorPosition(0, 3);
+Console.WriteLine("Distance - " + distanceBetween + " meters");
+
 await Task.Delay(1000);
 
 while (distance > 100)
@@ -45,25 +74,36 @@ while (distance > 100)
 
     distance -= (firstFriendSpeed + secondFriendSpeed) * time;
     count++;
-    time2 = time2 + time;
+    time2 += time;
 
     Console.Clear();
     Console.SetCursorPosition(1, 1);
     if (friend == 2)
     {
-        Console.SetCursorPosition(((0 + time2 * firstFriendSpeed) / x) + 1, 1);
-        Console.WriteLine("@");      
+        Console.SetCursorPosition(((0 + time2 * firstFriendSpeed) / scale) + 1, 1);
+        Console.WriteLine("@");
     }
     else
     {
-        Console.SetCursorPosition(((distance2 - time2 * secondFriendSpeed) / x) - 1, 1);
+        Console.SetCursorPosition(((distanceBetween - time2 * secondFriendSpeed) / scale) - 1, 1);
         Console.WriteLine("@");
     }
-    
-    Console.SetCursorPosition((0 + time2 * firstFriendSpeed) / x, 1);
-    Console.WriteLine("8");
-    Console.SetCursorPosition((distance2 - time2 * secondFriendSpeed) / x, 1);
-    Console.WriteLine("8");
+
+    Console.SetCursorPosition((0 + time2 * firstFriendSpeed) / scale, 1);
+    Console.WriteLine("▓");
+    Console.SetCursorPosition((distanceBetween - time2 * secondFriendSpeed) / scale, 1);
+    Console.WriteLine("▓");
+
+    for (int i = 0; i < distanceBetween / scale; i++)
+    {
+        Console.SetCursorPosition(i, 2);
+        Console.WriteLine("-");
+    }
+
+    Console.SetCursorPosition(0, 3);
+    Console.WriteLine("Distance - " + distanceBetween + " meters");
+    Console.WriteLine("");
+
 
     await Task.Delay(1000);
 }
